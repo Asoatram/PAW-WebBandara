2b5ff7dd3b10b2f731d3d9773d3f6a80d45eedf4
slwnyladis <salwanaylaadistri@mail.ugm.ac.id>
2024-09-22 20:39:39 +0700

    update flightRoutes

--- src/routes/flightRoutes.js
+++ src/routes/flightRoutes.js
@@ -1,28 +1,13 @@
 const express = require('express');
-const Flight = require('./models/Flight'); // Ganti dengan path yang sesuai
+const Flight = require('../controller/flightController.js'); // Ganti dengan path yang sesuai
 const router = express.Router();
 
-// Get all flights
-router.get('/api/flights/get', async (req, res) => {
-    try {
-        const flights = await Flight.find();
-        res.status(200).send(flights);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
+// Get flight by pages
+router.get('/api/flight', Flight.getPageFlight);
 
-// Get a flight by ID
-router.get('/api/flights/get/:id', async (req, res) => {
-    try {
-        const flight = await Flight.findById(req.params.id);
-        if (!flight) {
-            return res.status(404).send();
-        }
-        res.status(200).send(flight);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
+// Get a flight by airport ID
+router.get('/api/flight', Flight.getAllFlightBySearch);
+
+router.get('/api/flight/all', Flight.getAllFlight)
 
 module.exports = router;
